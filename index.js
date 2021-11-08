434c2dfd71dbe9dbc7a4721ab4e6a8b13d4ceef2
James Burns <jrburns96@aol.com>
2021-11-08 11:58:47 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    let change = document.getElementById("text")
+    change.textContent = "This is really cool!";
+  });
+
+  console.log("dom not loaded yet");
